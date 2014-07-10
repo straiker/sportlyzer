a4ef05e76497a9540366dd3a41860f75ac2a4e61
straiker <rait@raits-mbp.lan>
2014-07-10 12:35:49 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    var users = new Array();
     $.getJSON('http://api.randomuser.me/?randomapi&results=20&orderby=asc',function(data){
         /* sidebar user data*/
         $("#sidebar-header img").attr('src',data.results[0].user.picture);
@@ -7,16 +8,27 @@ $(document).ready(function(){
 
         /* users list */
         $.each(alphabet, function(i){
-            $("#alphabet ul").append("<li class='disabled'><a href='#"+alphabet[i]+"'>"+alphabet[i]+"</a>");
-            $("#contacts ul").append("<a name='"+alphabet[i]+"'></a><h2>"+alphabet[i]+"</h2>");
+            $("#alphabet ul").append("<li><a href='#"+alphabet[i]+"'>"+alphabet[i]+"</a>");
             for(var j = 0; j < data.results.length; j++){
                 var user = data.results[j].user;
+                users.push(new Array());
                 if(user.name.last.substring(0,1) == alphabet[i]){
+                    users[i].push(j);
+                }
+            }
+        });
+
+        $.each(users, function(i){
+            if(users[i] != ""){
+                console.log(alphabet[i]+" : "+users[i]+" - "+users[i].length);
+                $("#contacts ul").append("<a name='"+alphabet[i]+"'></a><h2>"+alphabet[i]+"</h2>");
+                $.each(users[i], function(j){
+                    var z = users[i][j];
                     $("#contacts ul").append(
                         $("<li></li>").append(
-                            "<img src='"+user.picture+"'><p>"+user.name.last+", "+user.name.first+"</p>"
-                    ));
-                }
+                            "<img src='"+data.results[z].user.picture+"'><p>"+data.results[z].user.name.last+", "+data.results[z].user.name.first+"</p>"
+                        ));
+                });
             }
         });
     });
